3c73ebd59f706a03897b18147a4ffa9dd768b67b
ejay17 <ejaysapitola9@gmail.com>
2026-05-13 21:27:42 +0800

    applying tailwind

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>SPortal</title>
+        <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body>
+
+        <div class="">
+
+        </div>
         <p>Hello SPortal</p>
 </body>
+
 </html>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+        content: [
+                "./**/*.{php,js,html}"
+        ],
+        theme: {
+                extend: {},
+        },
+        plugins: [],
+};
