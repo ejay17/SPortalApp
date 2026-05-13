03c645a87292b1ece7e5c91f3d56790df55bcdf1
ejay17 <ejaysapitola9@gmail.com>
2026-05-13 21:51:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,25 @@
 
 <body>
 
-        <div class="">
+        <div class="h-screen w-screen flex flex-col items-center justify-center">
+                <div class="w-[80%] h-[40vh] flex flex-col items-center justify-center gap-6">
+                        <div class="flex flex-col items-center justify-center">
+                                <p>Welcome to SPortal</p>
+                                <p>This is the way to your Sports.</p>
+                        </div>
+                        <form action=""
+                                class="w-full flex flex-col">
 
+                                <p>Log In</p>
+
+                                <label for="">E-mail</label>
+                                <input type="text">
+
+                                <label for="">Password</label>
+                                <input type="password">
+                        </form>
+                </div>
         </div>
-        <p>Hello SPortal</p>
 </body>
 
 </html>
